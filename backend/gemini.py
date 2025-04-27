a40e1eac9e191b4efdaf369c1fdb1ac10e1c844f
gaurang1404 <rshirodkar717@gmail.com>
2025-04-27 11:30:07 +0530

    changes gemini response

--- backend/gemini.py
+++ backend/gemini.py
@@ -8,21 +8,24 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel("gemini-2.0-flash")
 
+# Function to generate polite responses to customer reviews
 def generate_response(new_review: str, embedding: list):
     prompt = f"""
-You are a polite restaurant assistant.
+You are a polite and professional public service assistant.
 
 Customer review: "{new_review}"
-Reply to the customer professionally and courteously.
+Reply to the customer courteously, acknowledging their feedback, and providing any relevant guidance or steps for improvement.
 """
     res = model.generate_content(prompt)
     return res.text.strip()
 
+# Function to generate a summary of multiple public service reviews
 def generate_summary(reviews: list, embeddings: list):
+    # Combine all the reviews into a single paragraph for summary generation
     joined_reviews = "\n".join(f"- {rev}" for rev in reviews)
     print(joined_reviews)
     prompt = f"""
-Summarize the following customer reviews into 3-4 lines highlighting the key sentiments:
+Summarize the following customer reviews regarding public services into 5-6 lines , highlighting the key sentiments, areas for improvement, and any recurring themes , display point wise for better understanding.:
 
 {joined_reviews}
 """
